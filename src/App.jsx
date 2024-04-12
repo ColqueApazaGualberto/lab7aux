d0c1632547b7a2d96bd11d84b84fd13d0e4727f0
Gualberto Colque <gualbertocolque4@gmail.com.com>
2024-04-12 17:54:23 -0400

    segunda pregunta

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { List } from './components/List'
-
-import { WapperList } from './components/WrapperList'
+import { Button } from './components/Butoon'
 
 function App() {
+  const buttonText = "Count: ";
+
   return (
     <>
-      <WapperList/>
-      <List/> 
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Intento 1 :) </h1>
+      <Button text = {buttonText}/>
     </>
   )
 }
